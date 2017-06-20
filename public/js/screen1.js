25922658cd3eb44f0c0482e0469eaea05ac1c16e
Mitchell Rodríguez <mitrodle@gmail.com>
2017-06-20 18:03:33 -0500

    Anexando textos dinámicamente

--- public/js/screen1.js
+++ public/js/screen1.js
@@ -1,63 +1,23 @@
 'use strict';
 
-$(document).ready(function(){
-      $('.carousel.carousel-slider').carousel({fullWidth: true});
-    });
-
+$(document).ready(_=>{
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+});
 
 function Screen1(){
 	const container = $('<div class="container"></div>');
-	const carousel = $('<div class="carousel carousel-slider"></div>');
-
+	const carousel = $('<div class="carousel carousel-slider center" data-indicators="true"></div>');
 	const images = ['icon-people', 'happy-person', 'group-people'];
-/*	
-	$.each(images, function(){
+	const title = ['Paga a tus amigos', 'Sin número de cuenta', 'Gratis y seguro'];
+	const subtitle = ['Paga a quien quieras desde donde quieras sin usar efectivo'];
 
-		carousel.append($('<img src="img/icons/' + this + '.png">'));
-	}); */
 
 	for(var i = 0; i < images.length; i++){
 
-		const imagesContainer = $('<a class="carousel-item" href="#"></a>').append($('<img class="carousel-image" src="img/icons/' + images[i] + '.png">'))
+		const imagesContainer = $('<div class="carousel-item" href=" "></div>').append($('<img class="carousel-image '+images[i]+'" src="img/icons/' + images[i] + '.png">'))
 		carousel.append(imagesContainer);
 	}
 
 	container.append(carousel);
 	return container
 }
-/*
-function carousel(){
-
-	$('.carousel div:last').insertBefore('.carousel div:first');
-	carousel.css('margin-left', '-'+100+'%');
-
-	function next(){
-
-		carousel.animate({marginLeft:'-'+200+'%'},700,function(){
-			$('.carousel div:first').insertAfter('.carousel div:last');
-			carousel.css('margin-left', '-'+100+'%');
-		});
-
-	}
-
-	function previous(){
-		carousel.animate({marginLeft:0},700,function(){
-			$('.carousel div:last').insertBefore('.carousel div:first');
-			carousel.css('margin-left', '-'+100+'%');
-		});
-	}
-
-	$('.carousel-image').on('swipeleft', function(){
-		next();
-	});
-}
-
-$(document).ready(function(){
-
-	$('div.prueba').on('swipeleft', swipe);
-
-	function swipe(event){
-		$(event.target).addClass('swipe');
-	};
-})
-*/
