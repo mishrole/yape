5b0aa9091662926a4a56c01069a5caea157c13dc
Mitchell Rodríguez <mitrodle@gmail.com>
2017-06-20 17:27:14 -0500

    No owl, no swipe function

--- public/js/screen1.js
+++ public/js/screen1.js
@@ -3,7 +3,7 @@
 function Screen1(){
 	const container = $('<div class="container carousel-container"></div>');
 	const carousel = $('<div class="carousel"></div>');
-	const prueba = $('<p>Swipe me</p>');
+	const prueba = $('<div class="prueba"></div>');
 
 	const images = ['icon-people', 'happy-person', 'group-people'];
 /*	
@@ -18,22 +18,44 @@ function Screen1(){
 		carousel.append(imagesContainer);
 	}
 
-//	$('.carousel div:last').insertBefore('.carousel div:first');
-//	carousel.css('margin-left', '-'+100+'%');
-
-//	carousel.animate({marginLeft:'-'+200+'%'},700,function(){
-//		$('.carousel div:first').insertAfter('.carousel div:last');
-//		carousel.css('margin-left', '-'+100+'%');
-//	});
-
 	container.append(carousel);
 	container.append(prueba);
-	
-$(document).on('pageinit', function(event){
-	$(prueba).on('swipeleft', function(){
-		alert("Izquierda (next)");
-	})
-})
 
 	return container
 }
+/*
+function carousel(){
+
+	$('.carousel div:last').insertBefore('.carousel div:first');
+	carousel.css('margin-left', '-'+100+'%');
+
+	function next(){
+
+		carousel.animate({marginLeft:'-'+200+'%'},700,function(){
+			$('.carousel div:first').insertAfter('.carousel div:last');
+			carousel.css('margin-left', '-'+100+'%');
+		});
+
+	}
+
+	function previous(){
+		carousel.animate({marginLeft:0},700,function(){
+			$('.carousel div:last').insertBefore('.carousel div:first');
+			carousel.css('margin-left', '-'+100+'%');
+		});
+	}
+
+	$('.carousel-image').on('swipeleft', function(){
+		next();
+	});
+}
+
+$(document).ready(function(){
+
+	$('div.prueba').on('swipeleft', swipe);
+
+	function swipe(event){
+		$(event.target).addClass('swipe');
+	};
+})
+*/
