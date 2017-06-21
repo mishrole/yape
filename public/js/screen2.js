3f79636dcc0856511f87a1dd7d4cfd0db715ab47
Mitchell Rodríguez <mitrodle@gmail.com>
2017-06-21 12:45:59 -0500

    Validaciones - primera parte

--- public/js/screen2.js
+++ public/js/screen2.js
@@ -10,8 +10,13 @@ function Screen2(){
 	const subtitle = $('<p class="subtitle2 grey-text">Recibirás un SMS con un código de validación</p>');
 	const row2 = $('<div class="row"></div>');
 	const inputField = $('<div class="input-field col s10 offset-s1"></div>');
-	const input = $('<input id="phone" type="text" class="validate">');
-	const label = $('<label><img src="img/icons/phoneandnumber.png" class="phoneandnumber"></label>');
+	const input = $('<input id="phone" type="text" class="validate center-align" autocomplete="off">');
+	const label = $('<label class="label-icon" for="phone"><img src="img/icons/phoneandnumber.png" class="phoneandnumber"></label>');
+	const span = $('<span></span>');
+	const checkbox = $('<input type="checkbox" id="terms" class="filled-in">');
+	const labelCheckbox = $('<label for="terms">Acepto los <a>Términos y condiciones.</a></label>');
+	const row3 = $('<div class="row continue"></div>');
+	const next = $('<a class="waves-effect waves-light btn next col s6 offset-s3" disabled="true">Continuar</a>');
 
 	imgContainer.append(img);
 	textContainer.append(title);
@@ -21,8 +26,76 @@ function Screen2(){
 	inputField.append(input);
 	inputField.append(label);
 	row2.append(inputField);
+	span.append(checkbox);
+	span.append(labelCheckbox);
+	row2.append(span);
+	row3.append(next);
 	container.append(row);
 	container.append(row2);
+	container.append(row3);
+
+	input.on('keyup', (e) => {
+
+		const dataInput = input.val();
+
+		if(dataInput.length == 9){
+			if((checkbox).prop('checked') == true){
+				next.attr('disabled', false);
+			}else{
+				next.attr('disabled', true);
+			}
+		}else{
+			next.attr('disabled', true);
+		}
+
+	})
+/*
+		labelCheckbox.on('click', (e) => {
+			if((checkbox).prop('checked')){
+				alert("true, se desmarca")
+				console.log((checkbox).prop('checked'))
+			}else{
+				console.log((checkbox).prop('checked'))
+				alert("false, se marca")
+			}
+		})	
+		labelCheckbox.on('click', (e) => {
+			if((checkbox).prop('checked') == false){
+				console.log((checkbox).prop('checked'))
+			}
+		})
+		*/
+/*
+	let verifyTerms;
+
+	labelCheckbox.on('click', (e) => {
+
+		if((checkbox).prop('checked') == false){
+			verifyTerms = true;
+		}else{
+			verifyTerms = false;
+		}
+	})
+
+	*/
+/*
+	input.on('keyup', (e) => {
+
+		const dataInput = input.val();
+
+		if(dataInput.length == 9 && (checkbox).prop('checked') == true){
+			next.attr('disabled', false);
+			console.log((checkbox).prop('checked'))
+		}else if((checkbox).prop('checked') == false){
+			next.attr('disabled', true);	
+			console.log((checkbox).prop('checked'))
+		}
+		
+	})
+
+	*/
+
+
 
 	return container;
 }
