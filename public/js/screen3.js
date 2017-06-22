851b25dcd9fba23b8ab112bdc196f9687d3d0621
Mitchell Rodríguez <mitrodle@gmail.com>
2017-06-22 12:00:08 -0500

    Screen 3

--- public/js/screen3.js
+++ public/js/screen3.js
@@ -1,6 +1,29 @@
 'use strict';
 
-function Screen3(update){
+function countdown(span){
+	const phone = state.user
+	let counter = 22;
+	let interval = setInterval(function() {
+		counter--;
+		if (counter >= 0) {
+			span.text(' ' + counter);
+		}
+
+		if (counter === 0) {
+			$.post('api/resendCode',
+			{
+				"phone": phone,
+			}, (data) => {
+				state.code = data.data
+				console.log(state.code)
+			});
+
+			countdown(span);
+		}
+	}, 1000);
+}
+
+function Screen3(update, interval){
 	const container = $('<div class="container center"></div>');
 	const row = $('<div class="row"></div>');
 	const imgContainer = $('<div class="col s12"></div>');
@@ -11,24 +34,39 @@ function Screen3(update){
 	const row2 = $('<div class="row"></div>');
 	const row3 = $('<div class="row"></div>');
 	const inputField = $('<div class="input-field col s10 offset-s1"></div>');
-	const input = $('<input id="phone" type="text" class="validate center-align" autocomplete="off">');
-	const label = $('<label class="label-icon" for="phone"><img src="img/icons/lock.png" class="lock"></label>');
+	const input = $('<input id="code" type="text" class="validate center-align" autocomplete="off">');
+	const label = $('<label class="label-icon" for="code"><img src="img/icons/lock.png" class="img-extra-small"></label>');
+	const span = $('<span></span>');
+	const p = $('<p>Reintentar en </p>');
+	const clock = $('<img src="img/icons/clock.png" class="img-extra-small">');
 
 	imgContainer.append(img)
 	row.append(imgContainer);
-
 	textContainer.append(title);
 	textContainer.append(subtitle);
 	row2.append(textContainer);
-
 	inputField.append(input);
 	inputField.append(label);
+	p.append(clock);
+	p.append(span);
+	inputField.append(p);
 	row3.append(inputField);
-	
 	container.append(row);
 	container.append(row2);
 	container.append(row3);
 
+	countdown(span);
+
+	input.on('keyup', (interval) => {
+
+		const dataInput = input.val();
+
+		if(dataInput == state.code){
+			state.screen = "Screen 4"
+			update();
+			clearInterval(interval)
+		}
+	})
 
 	return container;
 }
