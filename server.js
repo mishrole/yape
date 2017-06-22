ada86f6ccac2164dc31b3a53fa32dfe48cbe7ceb
Mitchell Rodríguez <mitrodle@gmail.com>
2017-06-22 14:55:43 -0500

    Port

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ router.get('/', (req, res) => {
 
 app.use('/api',apiUsers(router,db));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log('Server running on port '+port+'!');
